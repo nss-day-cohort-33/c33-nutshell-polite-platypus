d03214d5a67966168f95d2f98fe4181b13cc5fc6
Samuel Birky <sbirky21@gmail.com>
2019-07-01 15:45:40 -0500

    added html and dom components

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,41 @@
-console.log("Your Webpack application is set up and ready to go. Please start writing code.")
+//Sign Up Component
+    let signUpComponent =
+    `<fieldset>
+    
+    <label>Name</label>
+    <input type="text"/>
+    
+    <label>Email</label>
+    <input type="text"/>
+    
+    <label>Password</label>
+    <input type="text"/>
+    
+    <button>Sign Up</button>
+    
+    </fieldset>
+    `
+
+//Login Component
+    let loginComponent =
+    `<fieldset>
+
+    <label>Email</label>
+    <input type="text"/>
+
+    <label>Password</label>
+    <input type="text"/>
+
+    <button>Login</button>
+
+    </fieldset>
+    `
+
+    document.querySelector("#logInContainer").innerHTML = loginComponent
+
+let logInContainer = document.querySelector("#logInContainer")
+
+    logInContainer.addEventListener("click", () => {
+        document.querySelector("#homeContainer").classList.remove("hidden")
+})
+
