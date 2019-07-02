57a1e7cd9f0526924d47d75cbfc2b5d79f7f0df7
Samuel Birky <sbirky21@gmail.com>
2019-07-02 15:57:27 -0500

    welcomePageRequirements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,41 +1,28 @@
-//Sign Up Component
+// //Sign Up Component
     let signUpComponent =
     `<fieldset>
-    
     <label>Name</label>
     <input type="text"/>
-    
     <label>Email</label>
     <input type="text"/>
-    
     <label>Password</label>
     <input type="text"/>
-    
     <button>Sign Up</button>
-    
     </fieldset>
     `
-
-//Login Component
+// //Login Component
     let loginComponent =
     `<fieldset>
-
     <label>Email</label>
     <input type="text"/>
-
     <label>Password</label>
     <input type="text"/>
-
-    <button>Login</button>
-
+    <button class="loginBTN">Login</button>
     </fieldset>
     `
+document.querySelector("#loginContainer").innerHTML = loginComponent
 
-    document.querySelector("#logInContainer").innerHTML = loginComponent
+let loginBTN = document.querySelector("#loginBTN")
 
-let logInContainer = document.querySelector("#logInContainer")
-
-    logInContainer.addEventListener("click", () => {
-        document.querySelector("#homeContainer").classList.remove("hidden")
+loginBTN.addEventListener("click", () => {
 })
-
