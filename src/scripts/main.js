dc4df94f1f364216490942a1c461c82a1a2513ce
Mark Greene <mrkgreene7@gmail.com>
2019-07-03 14:19:30 -0500

    tasks and dummy data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,49 @@
 console.log("Your Webpack application is set up and ready to go. Please start writing code.")
+
+const tasks = 
+    {
+    id: 1,
+    user_id: 1,
+    name: "Thomas",
+    text: "First task",
+    due_date: "date"
+}
+
+<button onclick="myFunction()"></button>
+
+<script>
+function myFunction() {
+  var x = document.createElement("INPUT");
+  x.setAttribute("type", "checkbox");
+  document.body.appendChild(x);
+}
+</script>
+
+
+let taskPage = document.querySelector("#taskContainer")
+
+function taskHTML(fakeName) {
+    console.log(fakeName)
+    console.log(fakeName.name)
+
+    return `
+    <div>
+        <title>Tasks</title>
+        <section>
+            <h2>${fakeName.name}</h2>
+            <p>${fakeName.text}</p>
+        </section>
+        <section>
+            <input type="text">Enter Task Here</input>
+            <button id="addTask">Add</button>
+        </section>
+    </div>
+        `
+}
+
+function taskToDOM(passingTask) {
+    taskPage.innerHTML = ""
+    taskPage.innerHTML += taskHTML(passingTask)
+}
+
+taskToDOM(tasks)  
