fdead9837914ac8b609e0204e7ce79b6603917a8
berkeley platte <berkeleyplatte989@gmail.com>
2019-07-03 10:08:39 -0500

    working on article DOM component

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -1,20 +1,30 @@
-let formBtns = document.querySelectorAll(".modalBtn");
-let bodyBlackout = document.querySelector(".bodyBlackout");
-formBtns.forEach(btn => {
-  btn.addEventListener("click", () => {
-    let { modalBtn } = btn.dataset;
-    // console.log("dataset", btn.dataset);
-    // console.log("btn", btn);
-    let modalWin = document.querySelector(`[data-formDes="${modalBtn}"]`);
-    // console.log(modalWin);
-    modalWin.classList.add("visible");
-    bodyBlackout.classList.add("blackedOut");
+function bringModalForm() {
+  let formBtns = document.querySelectorAll(".modalBtn");
+  let bodyBlackout = document.querySelector(".bodyBlackout");
+  formBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+      let { modalBtn } = btn.dataset;
+      // console.log("dataset", btn.dataset);
+      // console.log("btn", btn);
+      let modalWin = document.querySelector(`[data-formDes="${modalBtn}"]`);
+      // console.log(modalWin);
+      modalWin.classList.add("visible");
+      bodyBlackout.classList.add("blackedOut");
+    });
   });
-});
-let submitBtn = document.querySelectorAll(".submitBtn");
-submitBtn.forEach(btn => {
-  btn.addEventListener("click", () => {
-    bodyBlackout.classList.remove("blackedOut");
-    btn.parentNode.classList.remove("visible");
+}
+
+function dismissModalForm() {
+  let submitBtns = document.querySelectorAll(".submitBtn");
+  submitBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+      bodyBlackout.classList.remove("blackedOut");
+      btn.parentNode.classList.remove("visible");
+    });
   });
-});
+}
+
+function saveArtFormData() {
+    
+}
+
