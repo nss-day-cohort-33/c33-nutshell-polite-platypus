9f1a973803839b54200b457139e94686dc278045
Blake Davis <blavis21@gmail.com>
2019-07-08 09:32:21 -0500

    login form code

---
+++ src/scripts/events.js
@@ -0,0 +1,5 @@
+function makeEventEntryComponent() {
+    return `
+    
+    `
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,50 @@
-// //Sign Up Component
-    let signUpComponent =
-    `<fieldset>
-    <label>Name</label>
-    <input type="text"/>
-    <label>Email</label>
-    <input type="text"/>
-    <label>Password</label>
-    <input type="text"/>
-    <button>Sign Up</button>
-    </fieldset>
-    `
-// //Login Component
-    let loginComponent =
-    `<fieldset>
-    <label>Email</label>
-    <input type="text"/>
-    <label>Password</label>
-    <input type="text"/>
-    <button class="loginBTN">Login</button>
-    </fieldset>
-    `
-document.querySelector("#loginContainer").innerHTML = loginComponent
+// function that creates signup component
+function signUp() {
+    return `
+    <form action="" class="form">
 
-let loginBTN = document.querySelector("#loginBTN")
+            <label for="Username">Username</label>
+            <input type="text" name="username" id="userName" class="label" required>
 
-loginBTN.addEventListener("click", () => {
+            <label for="Email">Email</label>
+            <input type="text" name="email" id="emailAddress" class="label" required>
+
+            <label for="Password">Password</label>
+            <input type="text" name="password" id="loginPassword" class="label" required>
+
+        </form>
+        <button type="Button" id="signUp-btn">Register</button>
+        <p>Already have an account? <a id="signInLink" href="">Sign In</a></p>
+    `
+}
+
+function logIn() {
+    return `
+    <form>
+    <label for="conceptsCovered">Email</label>
+    <input type="text" name="email" id="emailAddress" class="label" required><br />
+
+    <label for="jornalEntry">Password</label>
+    <input type="text" name="password" id="loginPassword" class="label" required>
+    </form>
+    <button id="loginBtn">Login</button>
+    `
+}
+
+
+let domRepop = document.querySelector("#test")
+
+function printSignUpComponent() {
+    domRepop.innerHTML += signUp()
+}
+
+function printLoginComponent() {
+    domRepop.innerHTML = ""
+    domRepop.innerHTML += logIn()
+}
+
+document.querySelector("#signupLoginBtn").addEventListener("click", () => {
+    document.getElementById("signupLoginBtn").style.visibility="hidden"
+    printSignUpComponent()
 })
+
